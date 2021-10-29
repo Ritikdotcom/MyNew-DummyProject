0c8876914b19841ecbdff9d9edf016cce09c5c98
Ritikdotcom <ritikdhakate3@gmail.com>
2021-10-29 17:41:09 +0530

    commit2

--- app/src/main/java/com/rtech/myapplication/MainActivity.java
+++ app/src/main/java/com/rtech/myapplication/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +16,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = findViewById(R.id.btnPdf);
+        initView();
+        initInstance();
+        initClickListner();
         textView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,5 +26,61 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+
+    /*
+     * Created BY : Ritik Dhakate.
+     * Created ON : 29-10-2021 17:25
+     * Description : This is used to create for
+     * Updated BY :
+     * Updated ON :
+     * Description :
+     */
+    private void initView() {
+
+        try {
+
+        } catch (Exception exception) {
+            System.out.println("@@@@@@@ Exception==>" + exception.toString());
+            Log.d("@@@@ Exception==>", exception.toString());
+        }
+    }
+
+
+    /*
+     * Created BY : Ritik Dhakate.
+     * Created ON : 29-10-2021 17:24
+     * Description : This is used to create for
+     * Updated BY :
+     * Updated ON :
+     * Description :
+     */
+
+    private void initInstance() {
+                try {
+
+        } catch (Exception exception) {
+            System.out.println("@@@@@@@ Exception==>" + exception.toString());
+            Log.d("@@@@ Exception==>", exception.toString());
+        }
+    }
+
+    /*
+     * Created BY : Ritik Dhakate.
+     * Created ON : 29-10-2021 17:24
+     * Description : This is used to create for
+     * Updated BY :
+     * Updated ON :
+     * Description :
+     */
+    private void initClickListner() {
+
+        try {
+
+        } catch (Exception exception) {
+            System.out.println("@@@@@@@ Exception==>" + exception.toString());
+            Log.d("@@@@ Exception==>", exception.toString());
+        }
     }
 }
